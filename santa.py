347e879433b5837bd0745cc4abbcb7c9f4936e4b
Sean <sean@myglit.ch>
2020-12-18 09:01:35 -0600

    Undo last change

--- santa.py
+++ santa.py
@@ -11,7 +11,7 @@ while int(key) < 100:
     url = "http://%s:8000/api/%s" % (ip, str(key))
     r = session.get(url)
     output = r.json()
-    print(str(output['item_id'] + '\t' + output['q'])
+    print(str(output['item_id']) + '\t' + output['q'])
     key += 2
     if 'Error' not in output['q']:
         break
